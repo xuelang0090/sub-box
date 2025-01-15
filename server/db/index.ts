d77f07c27b2da57ec79ae6745ff1598d5e19995e
kleinrui <kleinrui@proton.me>
2025-01-16 00:41:25 +0800

    use cloudflare workers

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,7 +1,24 @@
-import "dotenv/config";
+import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { drizzle } from "drizzle-orm/d1";
 
-import { drizzle } from "drizzle-orm/libsql";
+import { env } from "@/env";
+import * as schema from "./schema";
 
-const db = drizzle(process.env.DATABASE_URL!);
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+  client?: D1Database;
+};
 
-export default db;
+export let client: D1Database | undefined;
+
+export const db = async () => {
+  /**
+   * Don't call getRequestContext() at the top level
+   */
+  client = globalForDb.client ?? (await getCloudflareContext()).env.DB;
+  if (env.NODE_ENV !== "production") globalForDb.client = client;
+  return drizzle(client, { schema });
+};
